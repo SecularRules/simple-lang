c8c9c54889e4b833990f573ca895ff1e3936a69b
Lars <lluscuere@gmail.com>
2025-02-26 20:11:56 +0100

    remove recursion

---
+++ model.py
@@ -0,0 +1,53 @@
+def value(x, data):
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    if x in characters:
+        return data[characters.index(x)]
+    else:
+        return int(x)
+ 
+def condition(a, condition, b):
+    if condition == "==":
+        return a == b
+    if condition == "!=":
+        return a != b
+    if condition == "<":
+        return a < b
+    if condition == "<=":
+        return a <= b
+    if condition == ">":
+        return a > b
+    if condition == ">=":
+        return a >= b
+ 
+def run(program):
+    length = len(program)
+    row = 0
+    characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    data = [0]*26
+    result = []
+    while True:
+        if row == length:
+            break
+        parts = program[row].split(" ")
+        if parts[0] == "PRINT":
+            result.append(value(parts[1], data))
+        if parts[0] == "MOV":
+            data[characters.index(parts[1])] = value(parts[2], data)
+        if parts[0] == "ADD":
+            data[characters.index(parts[1])] += value(parts[2], data)
+        if parts[0] == "SUB":
+            data[characters.index(parts[1])] -= value(parts[2], data)
+        if parts[0] == "MUL":
+            data[characters.index(parts[1])] *= value(parts[2], data)
+        if parts[0] == "JUMP":
+            row = program.index(parts[1]+":")
+            continue
+        if parts[0] == "IF":
+            if condition(value(parts[1], data), parts[2], value(parts[3], data)):
+                row = program.index(parts[5]+":")
+                continue
+        if parts[0] == "END":
+            break
+        row += 1
+    return result
+# Write your solution here
--- simple_lang.py
+++ simple_lang.py
@@ -1,35 +1,64 @@
 import string
-#structural pattern matching / match case
+
+def run(program): #What is good ordering of the code/functions here? below as that is logical, or above as its easy to read
+    #I feel like these need to be accessed and changed globally, but is that good practice?
+    #Feels like something is wrong with that, but also seems weird to pass them all through all functions
+    global printlist
+    printlist = []
+    global location
+    location = {} #save location name and index
+    global vars
+    vars = {char : 0 for char in string.ascii_uppercase} #initialise vars
+    global i
+    i = 0
+    build_dic(program) #gotta find locations first
+
+    eval_program(program, i)
+    return printlist
+
+def build_dic(program):
+    k = 0
+    for j in program:
+        j = j.split(" ")
+        location[j[0][:-1]] = k
+        k += 1
+    #print(location)
 
 def eval_program(program, i):
     if len(program) == 0: return #printlist #gotta avoid errors if its empty
-    line = program[i]
-    line = line.split(" ")
-    match line[0]:
-        case "PRINT":
-            printlist.append(vars[line[1]]) #here using another method is way more complex than fixing it here
-        case "MOV":
-            MOV(line[1], line[2])
-        case "ADD":
-            ADD(line[1], line[2])
-        case "SUB":
-            SUB(line[1], line[2])
-        case "MUL":
-            MUL(line[1], line[2])
-        case "JUMP":
-            eval_program(program,location[line[1]]) #doubt if should be function but needs pogram
-        case "IF":
-            if IF(line[1], line[2], line[3]):
-                eval_program(program,location[line[5]])
-        case "END":
-            return #printlist
-        case _: #catch all for the locations
-            location[line[0][:-1]] = i # also here doesnt seem logical to use another function?
+    while True:
+        line = program[i]
+        line = line.split(" ")
+        match line[0]:
+            case "PRINT":
+                PRINT(line[1])
+            case "MOV":
+                MOV(line[1], line[2])
+            case "ADD":
+                ADD(line[1], line[2])
+            case "SUB":
+                SUB(line[1], line[2])
+            case "MUL":
+                MUL(line[1], line[2])
+            case "JUMP":
+                i = location[line[1]] -1 #doubt if should be function but needs pogram
+            case "IF":
+                if IF(line[1], line[2], line[3]):
+                    i = location[line[5]] -1
+            case "END":
+                return #printlist
+            case _: #catch all for the locations
+                location[line[0][:-1]] = i # also here doesnt seem logical to use another function?
+        i += 1
+        if i >= len(program): return #printlist #avoid errors if there is no end statement
     
-    i += 1
-    if i >= len(program): return #printlist #avoid errors if there is no end statement
-    eval_program(program, i)
+def PRINT(a):
+    if a in vars:
+        printlist.append(vars[a])
+    else:
+        printlist.append(int(a))
     
+
 def MOV(a,b):
     if b in vars:
         vars[a] = vars[b]
@@ -74,31 +103,18 @@ def IF(val1, op, val2): #feels like this might be doable directly with like int(
         case "!=":
             return int(val1) != int(val2)
 
-def run(program): #What is good ordering of the code/functions here? below as that is logical, or above as its easy to read
-    #I feel like these need to be accessed and changed globally, but is that good practice?
-    #Feels like something is wrong with that, but also seems weird to pass them all through all functions
-    global printlist
-    printlist = []
-    global location
-    location = {} #save location name and index
-    global vars
-    vars = {char : 0 for char in string.ascii_uppercase} #initialise vars
-    i = 0
-    eval_program(program, i)
-    return printlist
-
 if __name__ == "__main__":
     program1 = []
     program1.append("MOV A 1")
-    program1.append("MOV B 1")
+    program1.append("MOV B 999")
     program1.append("start:")
-    program1.append("MUL A 2")
-    program1.append("ADD B 1")
-    program1.append("IF B != 101 JUMP start")
-    #program1.append("MUL A B")
-    #program1.append("JUMP potat")
-    program1.append("PRINT A")
-    program1.append("END")
+    program1.append("ADD A 1")
+    program1.append("SUB B 1")
+    program1.append("ADD C 1")
+    program1.append("IF A == B JUMP end")
+    program1.append("JUMP start")
+    program1.append("end:")
+    program1.append("PRINT C")
     
     result = run(program1)
     print(result)
